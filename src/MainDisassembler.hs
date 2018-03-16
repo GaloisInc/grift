2a38128ccec6bd202fb8c84d7197c5c4262c9f6a
Ben Selfridge <benselfridge@galois.com>
2018-03-16 15:18:06 -0700

    improved dis output

--- src/MainDisassembler.hs
+++ src/MainDisassembler.hs
@@ -39,42 +39,59 @@ main = do
 
   -- load in each file as a bytestring
   forM_ args $ \fileName -> do
-    fileBS <- BS.readFile fileName
-    case parseElf fileBS of
-      Elf32Res _err e -> disElf e
-      Elf64Res _err e -> disElf e
-      ElfHeaderError _byteOffset _msg -> do
-        putStrLn "error"
+    insts <- disFile fileName
+    forM_ insts $ \(inst, instBV) -> do
+      putStrLn $ show instBV ++ ": " ++ show inst
+
+-- | Read in an ELF file, find the code sections, and write them to a file as raw
+-- bytes
+asmFile :: String -> [BitVector 32] -> IO ()
+asmFile fileName bvs = do
+  let bvToBytes bv = [ bvExtract 0  bv :: BitVector 8
+                     , bvExtract 8  bv :: BitVector 8
+                     , bvExtract 16 bv :: BitVector 8
+                     , bvExtract 24 bv :: BitVector 8
+                     ]
+      bytes = BS.pack $ fromIntegral <$> bvIntegerU <$> concat (bvToBytes <$> bvs)
+  BS.writeFile fileName bytes
+
+disFile :: String -> IO [(Some Instruction, BitVector 32)]
+disFile fileName = do
+  fileBS <- BS.readFile fileName
+  case parseElf fileBS of
+    Elf32Res _err e -> disElf e
+    Elf64Res _err e -> disElf e
+    ElfHeaderError _byteOffset _msg -> return []
+
+disElf :: ElfWidthConstraints w => Elf w -> IO [(Some Instruction, BitVector 32)]
+disElf e = do
+  let codeSections = filter isCodeSection $  e ^.. elfSections
+      isCodeSection sec =
+        (elfSectionFlags sec .&. shf_execinstr) /= zeroBits
+      bss = elfSectionData <$> codeSections
+      dis = concat $ disInstructions <$> bss
+  return $ dis
 
 -- | Decode a single instruction from a bytestring and returns the instruction along
 -- with the remaining bytes and the number of bytes consumed. Since we currently only
 -- support RV32I, this function only decodes 4-byte words.
-disInstruction :: BS.ByteString -> Maybe (Some Instruction, BS.ByteString, Int)
+disInstruction :: BS.ByteString -> Maybe (Some Instruction, BitVector 32, BS.ByteString, Int)
 disInstruction bs =
-  bool Nothing (Just (inst, rstBytes, numBytes)) (BS.length bs >= 4)
+  bool Nothing (Just (inst, instBV, rstBytes, numBytes)) (BS.length bs >= 4)
   where (instBytes, rstBytes) = BS.splitAt 4 bs
         (b0:b1:b2:b3:[]) = fromIntegral <$> BS.unpack instBytes :: [Integer]
         instWordI = b3 `shiftL` 24 .|.
                     b2 `shiftL` 16 .|.
                     b1 `shiftL` 8  .|.
                     b0
-        inst = decode $ bitVector instWordI
+        instBV = bitVector instWordI
+        inst = decode instBV
         numBytes = 4
 
 -- | Decode a bytestring as a RISC-V program
-disInstructions :: BS.ByteString -> [Some Instruction]
+disInstructions :: BS.ByteString -> [(Some Instruction, BitVector 32)]
 disInstructions bs = case disInstruction bs of
   -- not enough bytes to disassemble an instruction. We should probably detect if
   -- length bs < 4 and give some kind of warning, but I'm not worried about that for now.
   Nothing -> []
-  Just (inst, bsRst, _) -> inst : disInstructions bsRst
-
-disElf :: ElfWidthConstraints w => Elf w -> IO ()
-disElf e = do
-  let codeSections = filter isCodeSection $  e ^.. elfSections
-      isCodeSection sec =
-        (elfSectionFlags sec .&. shf_execinstr) /= zeroBits
-  forM_ codeSections $ \sec -> do
-    let bs = elfSectionData sec
-    mapM_ print $ disInstructions bs
-    return ()
+  Just (inst, instBV, bsRst, _) -> (inst, instBV) : disInstructions bsRst
