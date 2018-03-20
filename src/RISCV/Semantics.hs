79e1ec59598cbc793248559c4949dc5d8cd36f97
Ben Selfridge <benselfridge@galois.com>
2018-03-20 16:08:16 -0700

    cleaned up semantics module

--- src/RISCV/Semantics.hs
+++ src/RISCV/Semantics.hs
@@ -10,7 +10,43 @@
 
 -- I'm doing something based on SemMC here. Might actually make sense to actually
 -- use SemMC, but I'm not sure yet.
-module RISCV.Semantics where
+
+{-|
+Module      : RISCV.Semantics
+Copyright   : (c) Benjamin Selfridge, 2018
+                  Galois Inc.
+License     : None (yet)
+Maintainer  : benselfridge@galois.com
+Stability   : experimental
+Portability : portable
+
+Several types and functions enabling specification of semantics for RISC-V
+instructions.
+-}
+
+module RISCV.Semantics
+  ( -- * Types
+    Arch(..)
+  , ArchWidth
+  , Parameter(..)
+  , BVExpr(..)
+  , Stmt(..)
+  , Formula(..)
+  -- * Semantics monad
+  , Semantics
+  , runSemantics
+  , litBV
+  , regRead
+  , memRead
+  , add
+  , ite
+  , comment
+  , assignReg
+  , assignMem
+  , assignPC
+  , condStmt
+  , param
+  ) where
 
 import Control.Lens ( (%=) )
 import Control.Lens.TH (makeLenses)
@@ -43,6 +79,8 @@ instance Show (Parameter arch w) where
   show (Parameter _repr name) = name
 instance ShowF (Parameter arch)
 
+-- | BitVector expressions. These are the building blocks for semantic formulas for
+-- an instruction.
 data BVExpr (arch :: Arch) (w :: Nat) where
   -- Basic constructors
   LitBV :: BitVector w -> BVExpr arch w
@@ -72,11 +110,13 @@ instance Show (BVExpr arch w) where
     "if (" ++ show t ++ ") then " ++ show e1 ++ " else " ++ show e2
 instance ShowF (BVExpr arch)
 
--- | A 'Stmt' represents an atomic state transformation.
+-- | A 'Stmt' represents an atomic state transformation -- typically, an assignment
+-- of a state component (register, memory location, etc.) to a 'BVExpr' of the
+-- appropriate width.
 data Stmt (arch :: Arch) where
   AssignReg :: BVExpr arch 5 -> BVExpr arch (ArchWidth arch) -> Stmt arch
-  AssignMem :: BVExpr arch (ArchWidth arch) -- ^ address
-            -> BVExpr arch (ArchWidth arch) -- ^ value
+  AssignMem :: BVExpr arch (ArchWidth arch)
+            -> BVExpr arch (ArchWidth arch)
             -> Stmt arch
   AssignPC  :: BVExpr arch (ArchWidth arch) -> Stmt arch
 
@@ -114,9 +154,12 @@ instance Show (Formula arch) where
           showParams = intercalate ", " (toList (show <$> params))
           showDefs = concat (toList ((\d -> "  " ++ show d ++ "\n") <$> defs))
 
+-- | Every definition begins with the empty formula.
 emptyFormula :: Formula arch
 emptyFormula = Formula Seq.empty Seq.empty Seq.empty
 
+-- | State monad for defining instruction semantics. When defining an instruction,
+-- you shouldn't need to ever read the state directly, so we only export the type.
 newtype Semantics arch a =
   Semantics { unSemantics :: State (Formula arch) a }
   deriving (Functor,
@@ -124,64 +167,70 @@ newtype Semantics arch a =
             Monad,
             MonadState (Formula arch))
 
+-- | Obtain the formula defined by a 'Semantics' action.
 runSemantics :: Semantics arch () -> Formula arch
 runSemantics = flip execState emptyFormula . unSemantics
 
-litBV :: BitVector w -> BVExpr arch w
-litBV = LitBV
-
-regRead :: BVExpr arch 5 -> BVExpr arch (ArchWidth arch)
-regRead = RegRead
-
-memRead :: BVExpr arch (ArchWidth arch)
-        -> BVExpr arch (ArchWidth arch)
-memRead = MemRead
-
-add :: BVExpr arch w -> BVExpr arch w -> BVExpr arch w
-add = Add
-
-ite :: BVExpr arch 1
-    -> BVExpr arch w
-    -> BVExpr arch w
-    -> BVExpr arch w
-ite = Ite
-
+-- | Add a comment.
 comment :: String -> Semantics arch ()
 comment c = fComment %= \cs -> cs Seq.|> c
 
+-- | Literal bit vector.
+litBV :: BitVector w -> Semantics arch (BVExpr arch w)
+litBV = return . LitBV
+
+-- | Read a register.
+regRead :: BVExpr arch 5 -> Semantics arch (BVExpr arch (ArchWidth arch))
+regRead = return . RegRead
+
+-- | Read a memory location.
+memRead :: BVExpr arch (ArchWidth arch)
+        -> Semantics arch (BVExpr arch (ArchWidth arch))
+memRead = return . MemRead
+
+-- | Add two expressions.
+add :: BVExpr arch w
+    -> BVExpr arch w
+    -> Semantics arch (BVExpr arch w)
+add e1 e2 = return (Add e1 e2)
+
+-- | Conditional branch.
+ite :: BVExpr arch 1
+    -> BVExpr arch w
+    -> BVExpr arch w
+    -> Semantics arch (BVExpr arch w)
+ite t e1 e2 = return (Ite t e1 e2)
+
+-- | Add a statement to the formula.
 addStmt :: Stmt arch -> Semantics arch ()
 addStmt stmt = fDef %= \stmts -> stmts Seq.|> stmt
 
+-- TODO: protect against multiple assignments? (for all of the assign* functions)
+-- | Add a register assignment to the formula.
 assignReg :: BVExpr arch 5
           -> BVExpr arch (ArchWidth arch)
           -> Semantics arch ()
 assignReg r e = addStmt (AssignReg r e)
 
+-- TODO: Should we allow arbitrary width assignments?
+-- | Add a memory location assignment to the formula.
 assignMem :: BVExpr arch (ArchWidth arch)
           -> BVExpr arch (ArchWidth arch)
           -> Semantics arch ()
 assignMem addr val = addStmt (AssignMem addr val)
 
+-- | Add a PC assignment to the formula.
 assignPC :: BVExpr arch (ArchWidth arch) -> Semantics arch ()
 assignPC pc = addStmt (AssignPC pc)
 
+-- | Add a conditional assignment to the formula.
 condStmt :: BVExpr arch 1 -> Stmt arch -> Semantics arch ()
 condStmt cond stmt = addStmt (CondStmt cond stmt)
 
 -- TODO: check that the parameter has not already been declared.
--- | Declare a parameter for use in a formula.
+-- | Declare a parameter for use in the formula.
 param :: KnownNat w => String -> Semantics arch (BVExpr arch w)
 param s = do
   let p = Parameter knownNat s
   fParams %= \params -> params Seq.|> (Some p)
   return (ParamBV p)
-
-addSem :: Semantics arch ()
-addSem = do
-  comment "Adds register x[rs2] to register x[rs1] and writes the result to x[rd]."
-  comment "Arithmetic overflow is ignored."
-  rd    <- param "rd"
-  x_rs1 <- regRead <$> param "rs1"
-  x_rs2 <- regRead <$> param "rs2"
-
-  assignReg rd (add x_rs1 x_rs2)
