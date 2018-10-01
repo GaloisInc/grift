63bd2b96e7930dc41c8c78429b258a377a7bc016
Ben Selfridge <benselfridge@galois.com>
2018-10-01 10:44:19 -0700

    failing C test currently

--- test/run-riscv-tests.sh
+++ test/run-riscv-tests.sh
@@ -1,10 +1,10 @@
 rm -rf isa-results/
 mkdir isa-results
 
-for file in riscv-tests/build/share/riscv-tests/isa/rv32ui-p-*
+for file in riscv-tests/build/share/riscv-tests/isa/rv64uc-p-*
 do
     [[ $file == *.dump ]] && continue
     f=$(basename $file)
     echo "testing $file..."
-    grift-sim --arch=RV32I --steps=1000 "$file" >> "./isa-results/$f.out"
+    grift-sim --arch=RV64GC --steps=1000 "$file" >> "./isa-results/$f.out"
 done
