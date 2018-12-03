d1cdb50051974bcd28ef08cd12b0819eb6da9a32
Ben Selfridge <benselfridge@galois.com>
2018-12-03 13:57:35 -0800

    port to new-build; part 1

--- test/riscv-compliance/test.sh
+++
@@ -1,7 +0,0 @@
-rm -rf results/
-mkdir results
-for file in *.elf
-do
-    echo "testing $file..."
-    grift-sim --arch=RV32IM --steps=1000000 --coverage="results/$file.log" "$file" > "results/$file.out"
-done
