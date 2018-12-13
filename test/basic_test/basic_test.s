e1cb33a021342e482aa8c6051742a81d7c83f653
Ben Selfridge <benselfridge@galois.com>
2018-12-13 15:57:24 -0800

    cleaned things up, split out grift-sim from core grift library

--- test/basic_test/basic_test.s
+++
@@ -1,9 +0,0 @@
-	.section ".text"
-	.globl main
-
-main:	li x4, 0xabcdabcd
-	li x5, 0x20000
-
-	sd x4, 0(x5)
-	ld x6, 0(x5)
-	ret
--- test/basic_test/build.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-set -x
-
-#riscv32-unknown-elf-gcc -static -nostdlib -nostartfiles -S qsort_main.c -o qsort_main32.s
-#riscv64-unknown-elf-gcc -static -nostdlib -nostartfiles -S qsort_main.c -o qsort_main64.s
-
-riscv64-unknown-elf-gcc -mcmodel=medany -march=rv64im -mabi=lp64 -static -nostdlib -nostartfiles init.S basic_test.s -o basic_test64
-riscv64-unknown-elf-objdump -D basic_test64 >basic_test64.text
-
